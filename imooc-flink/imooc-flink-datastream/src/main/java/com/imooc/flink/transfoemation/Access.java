c6f82dfc80aab5669f9ae5606a79493ad12c1210
jzh <576577486@qq.com>
2022-04-02 23:07:45 +0800

    flink:day01

---
+++ imooc-flink/imooc-flink-datastream/src/main/java/com/imooc/flink/transfoemation/Access.java
@@ -0,0 +1,40 @@
+package com.imooc.flink.transfoemation;
+
+public class Access {
+    private Long time;
+    private String domain;
+    private Double traffic;
+
+    public Access() {
+    }
+
+    public Access(Long time, String domain, Double traffic) {
+        this.time = time;
+        this.domain = domain;
+        this.traffic = traffic;
+    }
+
+    public Long getTime() {
+        return time;
+    }
+
+    public void setTime(Long time) {
+        this.time = time;
+    }
+
+    public String getDomain() {
+        return domain;
+    }
+
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
+
+    public Double getTraffic() {
+        return traffic;
+    }
+
+    public void setTraffic(Double traffic) {
+        this.traffic = traffic;
+    }
+}
